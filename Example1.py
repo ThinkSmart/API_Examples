aa55fe5187ea98711cff25e8b36940a0b12bea8b
Cameron Pierce <cameronericpierce@gmail.com>
2017-06-29 09:50:47 -0700

    Updated Example1
    Takes username and password from command line (note- doesn’t handle
    errors).

--- Example1.py
+++ Example1.py
@@ -1,12 +1,9 @@
-# resource_owner.py
 # written and tested in Python 3.6.0
-# last updated 06/26/17
+# last updated 06/29/17
 
+import getpass
 import requests
 import json
-import getpass
-
-print("Please enter your ThinkSmart password")
 
 # ---------------- #
 # Global Variables #
@@ -16,21 +13,19 @@ url_root = 'https://default.tap.thinksmart.com/prod/'
 client_id = 'b858d97c24124c959ec0d78f3eccd77d'
 client_secret = '0WDF4cRc4gtEEhOBXkCH6dTj1NU18L8iL6+i3jHXoR4='
 
-username = "sample@thinksmart.com"
-password = getpass.getpass()
+username = input("Please enter your TAP username: ")
+password = getpass.getpass("Enter your password: ")
 
 workflow_name = "CamTest"
-
 
 # --------- #
 # Functions #
 # --------- #
 
-
 def getToken(url_root, username, password, client_id, client_secret):
 	"""
 	Given: Environment, user credentials, and client info.
-	Return: Response of POST call for token.
+	Return: Access token, valid for 1 hour.
 	"""
 
 	# concatenate environment address with piece of URL specific to API
@@ -48,16 +43,16 @@ def getToken(url_root, username, password, client_id, client_secret):
 					'client_id' : client_id,
 					'client_secret' : client_secret}
 
-	# return POST call response
+	# make API call
 	r = requests.post(url, headers=headers, data=body)
-	token = json.loads(r.text)["access_token"]
-
-	return token
+	
+	# parse POST call response, return token
+	return json.loads(r.text)["access_token"]
 
 def getTemplateID(url_root, workflow_name, token):
 	"""
 	Given: Environment, name of workflow, and valid token.
-	Return: Response of GET call for ID of template.
+	Return: Template ID of workflow.
 	"""
 
 	# construct URL
@@ -68,15 +63,16 @@ def getTemplateID(url_root, workflow_name, token):
 	# needs token
 	headers = {'Authorization' : 'Bearer ' + token}
 
-	# return GET call response
+	# make API call
 	r = requests.get(url, headers=headers)
-	print(r)
+
+	# parse GET call response, return template ID
 	return json.loads(r.text).get('Items')[0].get('ID')
 
 def createWorkflow(url_root, template_id, token, body):
 	"""
 	Given: Environment, ID of workflow, valid token, and field names and values.
-	Return: Response of POST call to initiate workflow.
+	Return: None, makes POST call to initiate workflow.
 	"""
 	
 	# construct URL
@@ -86,14 +82,11 @@ def createWorkflow(url_root, template_id, token, body):
 	headers = {'Authorization' : 'Bearer ' + token,
 							'Content-Type' : 'application/json'}
 
-	# decode body into JSON
+	# encode body into JSON
 	json_body = json.dumps(body)
 
-	# return POST call response
-	return requests.post(url, headers=headers, data=json_body)
-
-
-
+	# make POST call
+	requests.post(url, headers=headers, data=json_body)
 
 # -------------- #
 # Function Calls #
@@ -103,22 +96,6 @@ token = getToken(url_root, username, password, client_id, client_secret)
 
 template_id = getTemplateID(url_root, workflow_name, token)
 
-answersToSubmit = {"element5": "Hello", "element6": "World"}
+body = {"element5": "Hello", "element6": "World", "element7": "Dogs"}
 
-createWorkflow(url_root, template_id, token, answersToSubmit)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+createWorkflow(url_root, template_id, token, body)
